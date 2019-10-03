b0b9a4c4f53a3dc911b29ff5a83be1a1519f3e08
senner008 <nielshtg@gmail.com>
2019-10-03 11:24:48 +0200

    WIP

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace scraping_mvc
         {
               Boolean isProduction = Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT") == "Production";
             services.AddDbContext<FoodItemsContext> (options =>
-                    options.UseSqlServer (Environment.GetEnvironmentVariable("MyDbConnection")));
+                    options.UseSqlServer (Configuration.GetConnectionString ("MyDbConnection"));
             services.AddControllersWithViews();
         }
 
