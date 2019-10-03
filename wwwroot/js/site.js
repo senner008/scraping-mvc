1ca23a05ac79cd9869c510c9bc792633aa8c35dd
senner008 <nielshtg@gmail.com>
2019-10-03 17:59:14 +0200

    WIP

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,31 +18,64 @@ function priceSearch() {
 function descriptionSearch() {
     var input = document.querySelector("#descriptionSearch");
     input.addEventListener("input", function(e) {
-        globalQuery.description = e.target.value
+        globalQuery.description = e.target.value.toLowerCase();
+        renderlist(filterQuery())
+    });
+}
+
+function categorySearch() {
+    var input = document.querySelector("#categorySearch");
+    input.addEventListener("input", function(e) {
+        console.log("dsd")
+        globalQuery.category = e.target.value.toLowerCase();
         renderlist(filterQuery())
     });
 }
 
 
+
+function isLunch() {
+    var input = document.querySelector("#lunchSelect");
+    input.addEventListener("change", function(e) {
+        var index = e.target.options[e.target.selectedIndex].value
+        globalQuery.isLunch = Number(index) === 1 ? false : true
+        console.log(globalQuery.isLunch)
+        renderlist(filterQuery())
+    });
+}
+
 var globalQuery = {
     isLunch : false,
-    priceMax : 0,
-    description : ""
+    priceMax : Number.MAX_VALUE,
+    description : "",
+    category: ""
 }
 
 function filterQuery() {
     return (globalQuery.isLunch ? lunchlist : foodlist)
         .filter(p => extractnumber(p.price) <= globalQuery.priceMax)
         .filter(d => d.description.includes(globalQuery.description))
+        .filter(d => {
+            console.log(d)
+           return  d.category.includes(globalQuery.category)
+        })
 }
 
 async function getList (url) {
     return fetch(url)
     .then(res => res.json())
+    .then(res => {
+        return res.map(item => {
+            for (i in item) {
+                item[i] = ((typeof item[i] == "string") ?  item[i].toLowerCase() : item[i]);
+            }
+            return item;
+        })
+    })
 }
 
 function arrayToList (arr) {
-    return arr.map(item => "<li class='list-group-item'>" + "<span class='li-title'>" + item.title + "</span>" + "<span>" + item.description + "</span>" + "<span>" + item.price + "</span>" + "</li>");
+    return arr.map(item => "<li class='list-group-item'>" + "<span class='li-category'>" + item.category  + "</span>" + "-  " + "<span class='li-title'>" + item.title + "</span>" + "<span class='li-description'>" + item.description + "</span>" + "<span class='li-price'>" + item.price + "</span>" + "</li>");
 }
 
 function renderlist (list) {
@@ -56,5 +89,7 @@ $(document).ready(async function() {
     renderlist(foodlist);
     priceSearch();
     descriptionSearch();
+    categorySearch();
+    isLunch();
 
 });
