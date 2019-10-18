363058d74cb420c276b30ca47132850096937d66
Niels Gregersen <nielshtg@gmail.com>
2019-10-18 03:08:32 +0200

    WIP

--- Program.cs
+++ Program.cs
@@ -6,20 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace scraping_mvc
 {
     public class Program
     {
+        public IHostingEnvironment HostingEnvironment { get; }
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (env == "Heroku" || env == "Production") {
+                 CreateWebHostBuilder(args).Build().Run();
+            } else {
+                 CreateWebHostBuilder_DEV(args).Build().Run();
+            }
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                // .UseUrls("https://*:5001")
                 .UseStartup<Startup>();
+
+        public static IWebHostBuilder CreateWebHostBuilder_DEV(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("https://*:5001")
+                .UseStartup<Startup>();
+
+       
     }
 }
